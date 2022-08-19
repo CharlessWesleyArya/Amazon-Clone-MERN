8aabca020ebee4d9dd91ffa22ac1b194365ce6ee
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-08-19 23:55:28 +0530

    added searchbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFhXDh8MZJNB5pWdzwrHFc3ll-22RvWAU",
+  authDomain: "clone-d5a71.firebaseapp.com",
+  projectId: "clone-d5a71",
+  storageBucket: "clone-d5a71.appspot.com",
+  messagingSenderId: "600844363496",
+  appId: "1:600844363496:web:0fae087e995ef5343e8b5d",
+  measurementId: "G-ZJ5WL50KVH",
+};
