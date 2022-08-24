dcdc09544204ab9c982a9d9a9e3bc5684360dd21
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-08-24 21:58:20 +0530

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Header from "./Header";
 import "./App.css";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="App">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        {/* Header */}
+        <Header />
+        {/* Home */}
+        <Home />
+      </div>
+    </Router>
   );
 }
 
