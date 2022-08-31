209b541ce164e87b15e2ac173ecff16f838849c7
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-08-31 22:36:08 +0530

    added login page and its styling

--- src/Login.js
+++ src/Login.js
@@ -11,6 +11,25 @@ function Login() {
           alt=""
         />
       </Link>
+      <div className="login_container">
+        <h1>Sign-In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" name="" value="" />
+          <h5>Password</h5>
+          <input type="password" name="" value="" />
+          <button className="login_signInButton" type="">
+            Sign In
+          </button>
+        </form>
+        <p>
+          This is just a fake Amazon Clone app developed to get Knowledge in the
+          E commerce application
+        </p>
+        <button className="login_registerButton" type="">
+          Create Amazon Account
+        </button>
+      </div>
     </div>
   );
 }
