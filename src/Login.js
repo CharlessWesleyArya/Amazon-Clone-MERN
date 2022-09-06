f651b06cf8a0046c46a239c7768222a4b23bb5f2
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-09-06 23:38:33 +0530

    added functionality to signin and create account buttons and redirected to the page

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 import "./Login.css";
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        navigate("/");
+      })
+      .catch((error) => alert(error.message));
+
+    //some fancy firebase login stuff
+  };
+  const register = (e) => {
+    e.preventDefault();
+    //some fancy firebase register
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //it successfully created a new user
+        if (auth) {
+          navigate("/");
+        }
+        console.log(auth);
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -15,10 +44,20 @@ function Login() {
         <h1>Sign-In</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" name="" value="" />
+          <input
+            type="text"
+            name=""
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" name="" value="" />
-          <button className="login_signInButton" type="">
+          <input
+            type="password"
+            name=""
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={signIn} className="login_signInButton">
             Sign In
           </button>
         </form>
@@ -26,7 +65,7 @@ function Login() {
           This is just a fake Amazon Clone app developed to get Knowledge in the
           E commerce application
         </p>
-        <button className="login_registerButton" type="">
+        <button onClick={register} className="login_registerButton" type="">
           Create Amazon Account
         </button>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+//due to update of firebase we have imported the required things of firebase as follows
+//but before we just import firebase from 'firebase'
 const firebaseConfig = {
   apiKey: "AIzaSyBFhXDh8MZJNB5pWdzwrHFc3ll-22RvWAU",
   authDomain: "clone-d5a71.firebaseapp.com",
@@ -8,3 +13,8 @@ const firebaseConfig = {
   appId: "1:600844363496:web:0fae087e995ef5343e8b5d",
   measurementId: "G-ZJ5WL50KVH",
 };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
