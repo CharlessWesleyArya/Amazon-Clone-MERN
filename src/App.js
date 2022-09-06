16e469fe7b1151e1338511fab9e4298f8c685fb9
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-09-06 23:53:47 +0530

    added dispatch functionality to add the user into store

--- src/App.js
+++ src/App.js
@@ -4,8 +4,30 @@ import Home from "./Home";
 import Checkout from "./Checkout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    //this will only run when the app component is loaded
+    auth.onAuthStateChanged((authUser) => {
+      console.log(`user is ${authUser}`);
+      if (authUser) {
+        //the user is logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     //BEM
     <Router>
